c915036739856d555f86a32f47fb61247dbc0554
TJ Zimmerman <tj@tjzimmerman.com>
2023-04-03 00:20:27 -0700

    fix: Move arg parsing to function

--- main.py
+++ main.py
@@ -2,22 +2,23 @@ import argparse, datetime, sys
 from getpass import getpass
 from stravacookies import StravaCookieFetcher
 
-def buildPrefix(argv):
-    validServers = ["a", "b", "c"]
-    validActivities = ["run", "ride", "winter", "water", "all"]
-    validColors = ["blue", "bluered", "purple", "hot", "gray"]
-
+def argParse(args):
     parser = argparse.ArgumentParser()
-    parser.add_argument("-s", dest="server", choices=validServers, default="a", help=f"server, one of {validServers} (default: %(default)s)")
-    parser.add_argument("-a", dest="activity", choices=validActivities, default="all", help=f"activity, one of {validActivities} (default: %(default)s)")
-    parser.add_argument("-c", dest="color", choices=validColors, default="hot", help=f"color, one of {validColors} (default: %(default)s)")
+    parser.add_argument("-s", dest="server", choices=["a", "b", "c"], default="a", help="server, one of a, b, c (default: %(default)s)")
+    parser.add_argument("-a", dest="activity", choices=["run", "ride", "winter", "water", "all"], default="all", help="activity, one of run, ride, winter, water, all (default: %(default)s)")
+    parser.add_argument("-c", dest="color", choices=["blue", "bluered", "purple", "hot", "gray"], default="hot", help="color, one of blue, bluered, purple, hot, gray (default: %(default)s)")
     parser.add_argument("-r", dest="resolution", type=int, default=256, help="resolution, an integer (default: %(default)s)")
     parser.add_argument("-t", dest="useTMSFormat", default=False, action="store_true", help="use TMS format")
     parser.add_argument("-o", dest="useOSMFormat", default=False, action="store_true", help="use OSM format")
-    parsedArgs = parser.parse_args(argv)
+    return parser.parse_args()
 
-    urlPrefix = "tms:https://heatmap-external-" if parsedArgs.useTMSFormat else "https://heatmap-external-"
-    urlPrefix += f"{{switch:a,b,c}}.strava.com/tiles-auth/{parsedArgs.activity}/{parsedArgs.color}/{{zoom}}/{{x}}/{{y}}.png?px={parsedArgs.resolution}&" if parsedArgs.useOSMFormat else f"{parsedArgs.server}.strava.com/tiles-auth/{parsedArgs.activity}/{parsedArgs.color}/{{z}}/{{x}}/{{y}}.png?px={parsedArgs.resolution}&"
+def buildPrefix(args):
+    servers = ["a", "b", "c"]
+    activities = ["run", "ride", "winter", "water", "all"]
+    colors = ["blue", "bluered", "purple", "hot", "gray"]
+
+    urlPrefix = "tms:https://heatmap-external-" if args.useTMSFormat else "https://heatmap-external-"
+    urlPrefix += f"{{switch:a,b,c}}.strava.com/tiles-auth/{args.activity}/{args.color}/{{zoom}}/{{x}}/{{y}}.png?px={args.resolution}&" if args.useOSMFormat else f"{args.server}.strava.com/tiles-auth/{args.activity}/{args.color}/{{z}}/{{x}}/{{y}}.png?px={args.resolution}&"
 
     return urlPrefix
 
@@ -37,7 +38,8 @@ def buildSuffix():
     return cookieString
 
 def main():
-    urlPrefix = buildPrefix(sys.argv[1:])
+    parsedArgs = argParse(sys.argv[1:])
+    urlPrefix = buildPrefix(parsedArgs)
     urlSuffix = buildSuffix()
 
     print("Your Strava Heatmap URL is:\n\n" + urlPrefix + urlSuffix)
@@ -46,4 +48,4 @@ def main():
     print(f"\nThis URL will expire on {cookieExpiration}")
 
 if __name__ == "__main__":
-   main()
+    main()
